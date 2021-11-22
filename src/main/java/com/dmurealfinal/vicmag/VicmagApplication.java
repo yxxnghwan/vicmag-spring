9e85ca1fad2bd737f92f49597c2fb9b2e20cdb4a
yxxnghwan <younghwan960@gmail.com>
2021-11-22 20:21:39 +0900

    set server time zone Asia/Seoul

--- src/main/java/com/dmurealfinal/vicmag/VicmagApplication.java
+++ src/main/java/com/dmurealfinal/vicmag/VicmagApplication.java
@@ -7,10 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class VicmagApplication {
     public static void main(String[] args) {
         SpringApplication.run(VicmagApplication.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
